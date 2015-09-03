fbf16d32c53960015d042dbcd7f90958f2cdea99
dnomadb <damon@mapbox.com>
2015-09-02 18:34:16 -0700

    0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs_open('README.rst', encoding='utf-8') as f:
 
 
 setup(name='untiler',
-      version='0.0.1',
+      version='0.0.2',
       description=u"Combine image tiles into composite TIFs",
       long_description=long_description,
       classifiers=[],
